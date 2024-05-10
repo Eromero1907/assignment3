857b11f298d87a2645432fb7370969fd44526afe
Eromero1907 <ejromeror@eafit.edu.co>
2024-05-09 21:08:57 -0500

    Creando método

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main{
     public static void main(String[] args) {
-    
+        tarea();
     }
     public static void tarea(){
         System.out.println("Assignment3 by Esteban Romero Ríos");
